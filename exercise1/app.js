cfda846beec1767bd840225e44d41520f2af35d3
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-04-04 07:23:47 +0000

    9章まで

---
+++ exercise1/app.js
@@ -0,0 +1,7 @@
+let int1 = 10;
+let int2 = 10;
+
+alert(`${int1}+${int2}=` + (int1 + int2));
+alert(`${int1}-${int2}=` + (int1 - int2));
+alert(`${int1}*${int2}=` + (int1 * int2));
+alert(`${int1}/${int2}=` + (int1 / int2));
